c2de19158e4c44c1f7e6d7305030770b70a3fe19
Adam Kearney <Adam Kearney>
2017-08-08 22:38:28 -0400

    styled leader board

--- my-angry-gf/Screens/Scores.js
+++ my-angry-gf/Screens/Scores.js
@@ -4,14 +4,74 @@ import {
   Text, 
   View, 
   TouchableOpacity, 
+  ActivityIndicator,
+  ListView
 } from 'react-native';
 
 export default class Scores extends Component {
+	constructor(props) {
+	    super(props);
+	    this.state = {
+	      isLoading: true
+	    }
+  }
+  componentDidMount() {
+    return fetch('https://radiant-river-84976.herokuapp.com/Scores')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        this.setState({
+          isLoading: false,
+          dataSource: ds.cloneWithRows(responseJson),
+        }, function() {
+          // do something with new state
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      })
+  }
 	render() {
-		return(
-			<View>
-	          <Text>Leader Board</Text>
-	        </View>
-		)
-	}
-};
+    if (this.state.isLoading) {
+      return (
+        <View style={{flex: 1, paddingTop: 20}}>
+          <ActivityIndicator />
+        </View>
+      );
+    }
+
+   return (
+      <View style={styles.scoreContainer}>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={(rowData) => 
+          	<View style={{flexDirection: 'row'}}>
+	          	<Text style={styles.playerText}>{rowData.playername}</Text>
+	          	<Text style={styles.scoreText}>{rowData.score}</Text>
+	          	<Text style={styles.numDays}>Days</Text>
+          	</View>
+          }
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+ 	scoreContainer: {
+ 		flex: 1,
+ 		paddingTop: 20,
+ 		alignItems: 'center',
+ 	},
+ 	playerText: {
+ 		fontSize: 20,
+ 	},
+ 	scoreText: {
+ 		fontSize: 20,
+ 		paddingLeft: 60,
+ 	},
+ 	numDays: {
+ 		fontSize: 20,
+ 		paddingLeft: 5,
+ 	}
+});
